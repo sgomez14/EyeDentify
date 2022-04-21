e96563dd363a37cf47cbc48fc3e571b753be9e12
Ze Yu <40028300+zey2@users.noreply.github.com>
2022-04-21 01:17:55 -0400

    bug fix

--- android_app/app/src/main/java/com/example/eyedentify/TagActivity.java
+++ android_app/app/src/main/java/com/example/eyedentify/TagActivity.java
@@ -160,7 +160,7 @@ public class TagActivity extends AppCompatActivity {
             //info array, [0] = img, [1] = description+keywords, [2] = audio
             String[] infoArray = message.split("%");
             if(infoArray.length == 3){
-                String[] info = sp.getString(infoArray[1], null).split("%");
+                String[] info = sp.getString(infoArray[1], null).split("%%%");
                 edtItemDescription.setText(info[0]);
                 edtItemKeywords.setText(info[1]);
 //                textToSpeech.speak(infoArray[2], TextToSpeech.QUEUE_FLUSH, null, null);
@@ -216,7 +216,7 @@ public class TagActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String unique = UUID.randomUUID().toString();
-                editor.putString(unique, edtItemDescription.getText()+"%"+edtItemKeywords.getText());
+                editor.putString(unique, edtItemDescription.getText()+"%%%"+edtItemKeywords.getText());
                 editor.commit();
                 String msg = "img%"+unique+"%"+(mFileName.equals("") ? "na" : mFileName);
                 startActivity(new Intent(TagActivity.this, NFCPairingActivity.class).putExtra("tagInfo", msg));
