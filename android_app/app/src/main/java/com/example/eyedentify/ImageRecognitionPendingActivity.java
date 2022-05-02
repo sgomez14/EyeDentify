aeb21137affdcbc961524fc2ff567083e3e09b6e
Ze Yu <40028300+zey2@users.noreply.github.com>
2022-05-02 19:29:29 -0400

    back button for pending activity

--- android_app/app/src/main/java/com/example/eyedentify/ImageRecognitionPendingActivity.java
+++ android_app/app/src/main/java/com/example/eyedentify/ImageRecognitionPendingActivity.java
@@ -2,6 +2,7 @@ package com.example.eyedentify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ImageRecognitionPendingActivity extends AppCompatActivity {
@@ -13,5 +14,11 @@ public class ImageRecognitionPendingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_recognition_pending);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(ImageRecognitionPendingActivity.this, MainActivity.class));
     }
 }
